f721d80bc34f0a8573788cbc79f6fd33cb2bbc02
chkouzin <charalampos.kouzinopoulos@cern.ch>
2015-02-18 16:07:04 +0100

    Initial implementation of a line Hough Transform algorithm

--- devices/hough/runHough.cxx
+++ devices/hough/runHough.cxx
@@ -2,6 +2,10 @@
 /// \brief Implementation of a cluster loader
 /// \author Charis Kouzinopoulos
 
+/// List of references: [1] Cheshkov, C. "Fast Hough-transform track reconstruction for the ALICE TPC." Nuclear
+/// Instruments and Methods in Physics Research Section A: Accelerators, Spectrometers, Detectors and Associated
+/// Equipment 566.1 (2006): 35-39.
+
 #include "AliHLTTPCTrackGeometry.h"
 #include "AliHLTTPCClusterDataFormat.h"
 #include "AliHLTTPCSpacePointContainer.h"
@@ -9,6 +13,8 @@
 #include "AliHLTTPCDefinitions.h"
 
 #include "TCanvas.h"
+#include "TH2F.h"
+#include "TGraph.h"
 #include "TGraph2D.h"
 
 #include "boost/filesystem.hpp"
@@ -17,8 +23,171 @@
 
 std::unique_ptr<AliHLTTPCSpacePointContainer> spacepoints;
 vector<float> clusterCoordinates;
+vector<unsigned int> accumulator;
+vector<int> trackCoordinates;
 
-void drawData(int totalNumberOfClusters, std::string dataFilename)
+int xMax = 0.0;
+int yMax = 0.0;
+int zMax = 0.0;
+
+int rMax;
+int rResolution;
+int thetaMax;
+
+int houghThreshold = 6;
+
+// sin and cos expect values in radians instead of degrees
+#define DEG2RAD 0.017453293f
+
+// We store 4 parameters, ID, X, Y, Z
+int clusterParameters = 4;
+
+// FIXME: convert float into double for bigger precision
+
+void setAccumulatorBin(int r, int theta)
+{
+  accumulator[r * thetaMax + theta]++;
+}
+
+int getAccumulatorBin(int r, int theta)
+{
+  return accumulator[r * thetaMax + theta];
+}
+
+void setTracks(int startX, int startY, int endX, int endY)
+{
+  trackCoordinates.push_back(startX);
+  trackCoordinates.push_back(startY);
+  trackCoordinates.push_back(endX);
+  trackCoordinates.push_back(endY);
+}
+
+int getTrackStartX(int trackNumber)
+{
+  return trackCoordinates[trackNumber * 4 + 0];
+}
+
+int getTrackStartY(int trackNumber)
+{
+  return trackCoordinates[trackNumber * 4 + 1];
+}
+
+int getTrackEndX(int trackNumber)
+{
+  return trackCoordinates[trackNumber * 4 + 2];
+}
+
+int getTrackEndY(int trackNumber)
+{
+  return trackCoordinates[trackNumber * 4 + 3];
+}
+
+int getNumberOfTracks()
+{
+  return trackCoordinates.size() / 4;
+}
+
+float getClusterX(int clusterNumber)
+{
+  return clusterCoordinates[clusterNumber * clusterParameters + 1];
+}
+
+float getClusterY(int clusterNumber)
+{
+  return clusterCoordinates[clusterNumber * clusterParameters + 2];
+}
+
+float getClusterZ(int clusterNumber)
+{
+  return clusterCoordinates[clusterNumber * clusterParameters + 3];
+}
+
+void drawTracks()
+{
+  TCanvas* c2 = new TCanvas("c2", "Reconstructed tracks", 0, 0, 800, 600);
+  TGraph* gr = new TGraph();
+  Double_t x[2], y[2];
+
+  cout << "Number of tracks: " << getNumberOfTracks() << endl;
+
+  for (int i = 0; i < getNumberOfTracks(); i++) {
+    x[0] = getTrackStartX(i);
+    x[1] = getTrackEndX(i);
+    y[0] = getTrackStartY(i);
+    y[1] = getTrackEndY(i);
+
+    if (i == 0) {
+      gr->DrawGraph(2, x, y, "AC");
+    } else {
+      gr->DrawGraph(2, x, y, "CP");
+    }
+  }
+  c2->Print("tracks.pdf");
+}
+
+void drawAccumulatorCurves(int totalNumberOfClusters)
+{
+  // FIXME: The method calculates again r. Is it possible to obtain this information directly from the accumulator?
+  TCanvas* c4 = new TCanvas("c4", "Accumulator curves", 0, 0, 800, 600);
+  TGraph* g[totalNumberOfClusters];
+
+  for (Int_t i = 0; i < totalNumberOfClusters; i++) {
+    float x = getClusterX(i);
+    float y = getClusterY(i);
+
+    g[i] = new TGraph();
+
+    for (Int_t theta = 0; theta < thetaMax; theta++) {
+      double r = (x * cos(theta * DEG2RAD)) + (y * sin(theta * DEG2RAD));
+      g[i]->SetPoint(i * totalNumberOfClusters + theta, theta, r);
+    }
+
+    g[i]->SetMarkerStyle(1);
+
+    if (i == 0) {
+      g[i]->Draw("AC");
+    } else {
+      g[i]->Draw("CP");
+    }
+  }
+  c4->Print("accumulatorCurves.pdf");
+}
+
+void drawAccumulatorHistogram(int totalNumberOfClusters)
+{
+  TCanvas* c5 = new TCanvas("c5", "Accumulator histogram", 0, 0, 800, 600);
+  TH2F* h =
+    new TH2F("h", "Accumulator histogram", 2 * rMax * rResolution, 0, 2 * rMax * rResolution, thetaMax, 0, thetaMax);
+
+  h->SetFillColor(46);
+
+  for (Int_t r = 0; r < 2 * rMax * rResolution; r++) {
+    for (Int_t theta = 0; theta < thetaMax; theta++) {
+      h->SetBinContent(r, theta, getAccumulatorBin(r, theta));
+    }
+  }
+
+  h->Draw("LEGO");
+  c5->Print("accumulatorHistogram.pdf");
+}
+
+void drawClusters1D(int totalNumberOfClusters)
+{
+  TCanvas* c3 = new TCanvas("c1", "Clusters", 0, 0, 800, 600);
+  TGraph* gr3 = new TGraph();
+
+  for (Int_t i = 0; i < totalNumberOfClusters; i++) {
+    gr3->SetPoint(i, getClusterX(i), getClusterY(i));
+  }
+
+  // Draw with colored dots
+  gr3->SetMarkerStyle(1);
+  gr3->Draw("A*");
+
+  c3->Print("clusters.pdf");
+}
+
+void drawClusters(int totalNumberOfClusters, std::string dataFilename)
 {
   TCanvas* c1 = new TCanvas("c1", dataFilename.c_str(), 0, 0, 800, 600);
   TGraph2D* dt = new TGraph2D(10000);
@@ -32,6 +201,130 @@ void drawData(int totalNumberOfClusters, std::string dataFilename)
   dt->Draw("pcol");
 
   c1->Print("clusters.pdf");
+}
+
+// Determine if the current value is the maximum in an area of 9x9 points
+int localAccumulatorMaxima(int r, int theta)
+{
+  int max = getAccumulatorBin(r, theta);
+
+  for (int deltaR = -4; deltaR < 5; deltaR++) {
+    for (int deltaTheta = -4; deltaTheta < 5; deltaTheta++) {
+      if ((deltaR + r >= 0 && deltaR + r < rMax) && (deltaTheta + theta >= 0 && deltaTheta + theta < thetaMax)) {
+        if (getAccumulatorBin(r + deltaR, theta + deltaTheta) > max) {
+          return getAccumulatorBin(r + deltaR, theta + deltaTheta);
+        }
+      }
+    }
+  }
+  return max;
+}
+
+void calculateTracks(int totalNumberOfClusters)
+{
+  /*  // DEBUG
+    for (Int_t r = 0; r < 2 * rMax * rResolution; r++) {
+      cout << r << ": " << setw(3);
+
+      for (Int_t theta = 0; theta < thetaMax; theta++) {
+        cout << theta << ": " << getAccumulatorBin(r, theta) << " ";
+      }
+      cout << endl;
+    }
+  */
+  int startX, startY, endX, endY;
+
+  for (Int_t r = 0; r < 2 * rMax * rResolution; r++) {
+    for (Int_t theta = 0; theta < thetaMax; theta++) {
+      if (getAccumulatorBin(r, theta) >= houghThreshold) {
+         if (localAccumulatorMaxima(r, theta) > getAccumulatorBin(r, theta)) {
+          continue;
+        }
+        //cout << "Found a track at [" << ((double)r / rResolution) - rMax << "][" << theta << "]" << endl;
+        if (theta >= 45 && theta <= 135) {
+          startX = 0;
+          startY = (((double)r / rResolution) - rMax - startX * cos(theta * DEG2RAD)) / sin(theta * DEG2RAD);
+          endX = xMax;
+          endY = (((double)r / rResolution) - rMax - endX * cos(theta * DEG2RAD)) / sin(theta * DEG2RAD);
+        } else {
+          startY = 0;
+          startX = (((double)r / rResolution) - rMax - startY * sin(theta * DEG2RAD)) / cos(theta * DEG2RAD);
+          endY = yMax;
+          endX = (((double)r / rResolution) - rMax - endY * sin(theta * DEG2RAD)) / cos(theta * DEG2RAD);
+        }
+        //cout << "Track coordinates: " << startX << "," << startY << " - " << endX << "," << endY << endl;
+        setTracks(startX, startY, endX, endY);
+      }
+    }
+  }
+}
+
+void conformalMapping(int totalNumberOfClusters)
+{
+  for (int i = 0; i < totalNumberOfClusters; i++) {
+    float x = getClusterX(i);
+    float y = getClusterY(i);
+
+    // Equation (2) from paper [1]
+    float alpha = x / (x * x + y * y);
+    float beta = y / (x * x + y * y);
+
+    cout << "A: " << alpha << " B: " << beta << endl;
+  }
+}
+
+void transform(int totalNumberOfClusters)
+{
+  thetaMax = 180;
+  // Trigonometrically, the maximum distance is designated by the square root of the summation of the squares of the x
+  // and y dimensions
+  rMax = ceil(sqrt(xMax * xMax + yMax * yMax));
+
+  // By setting rResolution = 1, r = 18.6 and r = 19.2 for theta = 48 will both cause bin(19,48) to increase. With a
+  // resoution of rResolution = 10, they will cause bin(186,48) and bin(192,48) to increase respectively
+  rResolution = 100;
+
+  // The lines will have -rMax <= r <= rMax and 0 <= theta <= thetaMax. The total space needed is thus 2 * rMax *
+  // thetaMax
+  accumulator.resize(2 * rMax * thetaMax * rResolution, 0);
+
+  for (Int_t i = 0; i < totalNumberOfClusters; i++) {
+    float x = getClusterX(i);
+    float y = getClusterY(i);
+
+    for (Int_t theta = 0; theta < thetaMax; theta++) {
+      double r = (x * cos(theta * DEG2RAD)) + (y * sin(theta * DEG2RAD));
+      //      cout << "x: " << x << " y: " << y << " theta: " << theta << " r: " << r
+      //           << " round r: " << round((r + rMax) * rResolution) << endl;
+
+      // Use a discreet value for r by rounding r. Then, rMax is added to r to change its range from -rMax <= r <= rMax
+      // to 0 <= r <= 2 * rMax
+      setAccumulatorBin(round((r + rMax) * rResolution), theta);
+    }
+  }
+}
+
+/// Determine the maximum ceiling to X,Y,Z coordinates from the clusterCoordinates vector to later allocate the hough
+/// transform accumulator
+void determineMaxDimensions(int totalNumberOfClusters)
+{
+  xMax = 0;
+  yMax = 0;
+  zMax = 0;
+
+  for (Int_t i = 0; i < totalNumberOfClusters; i++) {
+    if (abs(clusterCoordinates[i * 4 + 1]) > xMax) {
+      xMax = ceil(abs(clusterCoordinates[i * 4 + 1]));
+    }
+    if (abs(clusterCoordinates[i * 4 + 2]) > yMax) {
+      yMax = ceil(abs(clusterCoordinates[i * 4 + 2]));
+    }
+    if (abs(clusterCoordinates[i * 4 + 3]) > zMax) {
+      zMax = ceil(abs(clusterCoordinates[i * 4 + 3]));
+    }
+  }
+
+  //cout << "xMax: " << xMax << " yMax: " << yMax << " zMax: " << zMax << endl;
 }
 
 void printData(int totalNumberOfClusters)
@@ -162,9 +455,56 @@ int main(int argc, char** argv)
 
   cout << "Added " << totalNumberOfClusters << " clusters from " << totalNumberOfDataFiles << " data files" << endl;
 
-  // printData(totalNumberOfClusters);
+  // DEBUG
+  totalNumberOfClusters = 15;
+  for (int kk = 0; kk < 6; kk++) {
 
-  drawData(totalNumberOfClusters, dataFilename);
+    clusterCoordinates[kk * 4] = kk;
+    clusterCoordinates[kk * 4 + 1] = 9.0 + kk;
+    clusterCoordinates[kk * 4 + 2] = 17.0;
+    clusterCoordinates[kk * 4 + 3] = 0;
+  }
+
+  for (int kk = 6; kk < 11; kk++) {
+
+    clusterCoordinates[kk * 4] = kk;
+    clusterCoordinates[kk * 4 + 1] = 9.0 + kk - 5;
+    clusterCoordinates[kk * 4 + 2] = 17.0 + kk - 5;
+    clusterCoordinates[kk * 4 + 3] = 0;
+  }
+
+  for (int kk = 11; kk < 15; kk++) {
+    clusterCoordinates[kk * 4] = kk;
+    clusterCoordinates[kk * 4 + 1] = 10.0;
+    clusterCoordinates[kk * 4 + 2] = 19.0 + kk - 11;
+    clusterCoordinates[kk * 4 + 3] = 0;
+  }
+
+  //printData(totalNumberOfClusters);
+
+  // drawClusters(totalNumberOfClusters, dataFilename);
+  drawClusters1D(totalNumberOfClusters);
+
+  // Transform the cartesian coordinate system into the conformal mapping system
+  // conformalMapping(totalNumberOfClusters);
+
+  // Determine the maximum dimensions of the clusters for the accumulator
+  determineMaxDimensions(totalNumberOfClusters);
+
+  // Perform the hough transform on the clusters
+  transform(totalNumberOfClusters);
+
+  // Determine if any lines were found
+  calculateTracks(totalNumberOfClusters);
+
+  // Draw the accumulator data as curves
+  drawAccumulatorCurves(totalNumberOfClusters);
+
+  // Draw the accumulator data as a histogram
+  drawAccumulatorHistogram(totalNumberOfClusters);
+
+  // Draw the reconstructed tracks
+  drawTracks();
 
   return 0;
 }
